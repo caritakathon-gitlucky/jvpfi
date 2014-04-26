94f7c531ac229909dea5ab3c8101a60e1c05ce51
Adrian <me@adrianenriquez.com>
2014-04-26 18:50:04 +0800

    comment

--- public_html/assets/js/core.js
+++ public_html/assets/js/core.js
@@ -2,11 +2,14 @@ $(document).ready(function() {
  
   $("#owl-slider").owlCarousel({
  
-      navigation : true, // Show next and prev buttons
-      slideSpeed : 300,
-      paginationSpeed : 400,
-      singleItem:true
- 
+      navigation : false, // Show next and prev buttons
+      slideSpeed : 2000,
+      paginationSpeed : 800,
+      singleItem: true,
+      autoPlay: true,
+      pagination: false,
+      paginationNumbers: false,
+      autoHeight: true
       // "singleItem:true" is a shortcut for:
       // items : 1, 
       // itemsDesktop : false,
